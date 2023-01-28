0200d642b1c629c1dac060cd8723be5562a83da6
Jordy <msjung@injewelme.com>
2023-01-28 18:39:38 +0900

    AnswerRepository 작성 완료

--- src/main/java/portfolio/project/hashtagqna/repository/AnswerRepository.java
+++ src/main/java/portfolio/project/hashtagqna/repository/AnswerRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import portfolio.project.hashtagqna.entity.Answer;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long>, AnswerRepositoryCustom{
+    public Answer findAnswerById(Long id);
 }

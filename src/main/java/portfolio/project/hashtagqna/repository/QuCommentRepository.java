ed86cd4a8ff77e29047b9465dcdab71688ed939c
Jordy <msjung@injewelme.com>
2023-01-28 18:35:36 +0900

    작성 완료

--- src/main/java/portfolio/project/hashtagqna/repository/QuCommentRepository.java
+++ src/main/java/portfolio/project/hashtagqna/repository/QuCommentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import portfolio.project.hashtagqna.entity.QuComment;
 
 public interface QuCommentRepository extends JpaRepository<QuComment, Long>, QuCommentRepositoryCustom{
+    public QuComment findQuCommentById(Long id);
 }

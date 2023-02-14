3d824fd90709f51593be6e25e8ee617200b5299a
Jordy <msjung@injewelme.com>
2023-02-14 14:52:37 +0900

    service 완료

--- src/main/java/portfolio/project/hashtagqna/service/HashtagService.java
+++ src/main/java/portfolio/project/hashtagqna/service/HashtagService.java
@@ -3,13 +3,10 @@ package portfolio.project.hashtagqna.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import portfolio.project.hashtagqna.dto.HashtagDto;
 import portfolio.project.hashtagqna.entity.Hashtag;
 import portfolio.project.hashtagqna.entity.Member;
-import portfolio.project.hashtagqna.entity.Question;
-import portfolio.project.hashtagqna.entity.QuestionHashtag;
 import portfolio.project.hashtagqna.repository.HashtagRepository;
-import portfolio.project.hashtagqna.repository.QuestionHashtagRepository;
-import portfolio.project.hashtagqna.repository.QuestionRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +16,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HashtagService {
     private final HashtagRepository hashtagRepository;
-    private final QuestionRepository questionRepository;
-    private final QuestionHashtagRepository questionHashtagRepository;
 
+    public List<HashtagDto> viewAllHashtags() {
+        return hashtagRepository.viewAllHashtags();
+    }
 
+    public List<HashtagDto> choiceHashtags(List<Hashtag> hashtags) {
+        return hashtagRepository.findAllSelectedHashtags(hashtags);
+    }
+
+    public List<HashtagDto> viewAllMyHashtags(Member member) {
+        return hashtagRepository.viewMyAllHashtags(member);
+    }
 }

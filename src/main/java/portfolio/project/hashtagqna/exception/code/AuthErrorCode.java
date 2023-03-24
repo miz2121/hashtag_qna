f7257edf68cbc5347d50feefcb57a018dffb32cd
minsoo <miz2121@naver.com>
2023-03-24 16:57:01 +0900

    예외 처리 코드 한 줄 추가

--- src/main/java/portfolio/project/hashtagqna/exception/code/AuthErrorCode.java
+++ src/main/java/portfolio/project/hashtagqna/exception/code/AuthErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorCode implements ErrorCode{
     CLOSED_QUESTION_AUTH(HttpStatus.UNAUTHORIZED, "Can not add answer to closed question"),
     EDIT_QUESTION_AUTH(HttpStatus.UNAUTHORIZED, "Only question writer can delete or update the question"),
+    EDIT_ANSWER_AUTH(HttpStatus.UNAUTHORIZED, "Only answer writer can delete or update the answer"),
     EDIT_COMMENT_AUTH(HttpStatus.UNAUTHORIZED, "Only comment writer can delete or update the comment"),
     ANSWER_AUTH(HttpStatus.UNAUTHORIZED, "Question writer can not add an answer"),
     SELECT_AUTH(HttpStatus.UNAUTHORIZED, "Only question writer can select the answer"),
